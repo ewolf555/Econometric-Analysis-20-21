2bd49dff3dbf0119a528d3bb26677948540d1044
E.Wolf <74426565+ewolf555@users.noreply.github.com>
2020-11-13 19:42:32 +0100

    Update Tutorial 5 - Exercise 2.R

--- Tutorial 5 - Exercise 2.R
+++ Tutorial 5 - Exercise 2.R
@@ -5,16 +5,6 @@ library(car)
 # Import data
 ex_12 <- read_excel("...")
 
-# OLS Estimation 
-model_ols <- lm(INFLAT ~ MONEY + OUTPUT, data = ex_12)
-
-summary(model_ols)
-
-
-# F Test for the joint hypothesis
-linearHypothesis(model_ols, c("(Intercept) = 0", "MONEY = 1", "OUTPUT = -1"), test = "F")
-
-
 #IV Estimation
 model_ivreg <- ivreg(INFLAT ~ MONEY + OUTPUT | MONEY + INV + POPRATE + SCHOOL + INITIAL, data = ex_12)
 

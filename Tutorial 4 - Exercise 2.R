0d52c041341578a01e440e78e629c49b0c5dbd59
E.Wolf <74426565+ewolf555@users.noreply.github.com>
2020-11-13 19:48:56 +0100

    Update Tutorial 4 - Exercise 2.R

--- Tutorial 4 - Exercise 2.R
+++ Tutorial 4 - Exercise 2.R
@@ -6,24 +6,26 @@ library(sandwich)
 setwd("...")
 data_phillips <- read.csv("phillips.csv")
 
-
+# Estimate Model with OLS
 model_ols <- lm(inf ~ du, data = data_phillips)
 summary(model_ols)
 
+# Test for Autocorrelation
 plot(model_ols$residuals, type="l")
 acf(model_ols$residuals)
 bgtest(inf ~ du, order = 8, data = data_phillips)
 
-# Estimate With GLS
+# Estimate Model with GLS
 
+# Estimate the Corrleation Structure of Residuals 
 ar_res <- ar(model_ols$residuals, aic = TRUE)
 print(ar_res)
 
+# Estimate Model 
 model_gls <- gls(inf ~ du, correlation = corARMA(ar_res$ar, p=3), data = data_phillips)
-
 summary(model_gls)
 
-# Estimate with HAC Standard Errors
+# Estimate and compute the HAC Standard Errors
 hac_errors <- vcovHAC(model_ols)
 
 coeftest(model_ols, vcov. = hac_errors)

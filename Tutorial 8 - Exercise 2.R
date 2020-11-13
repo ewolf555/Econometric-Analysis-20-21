501f74d553ce6c76c4425ea3f09612249ecfd693
E.Wolf <74426565+ewolf555@users.noreply.github.com>
2020-11-13 19:41:41 +0100

    Update Tutorial 8 - Exercise 2.R

--- Tutorial 8 - Exercise 2.R
+++ Tutorial 8 - Exercise 2.R
@@ -1,23 +1,24 @@
 library(readr)
 library(systemfit)
 
+# Import data
 setwd("...")
-
 demand_supply_data <- read_csv("ds_data.csv")
 
-# Estimate Demand Equation with 3SLS
+# Define System of equations
 eqDemand <- quantity ~ price + income + demand_shifts
 eqSupply <- quantity ~ price + input_1 + input_2
 system <- list( demand = eqDemand, supply = eqSupply )
 
-
+# Define Instruments
 inst <- ~ income + demand_shifts + input_1 + input_2
 
+# Estimate Model with 2SLS
 fit2sls <- systemfit( system, "2SLS", inst = inst, data = demand_supply_data)
 
 summary(fit2sls)
 
-
+# Estimate Model with 3SLS
 fit3sls <- systemfit( system, "3SLS", inst = inst, data = demand_supply_data)
 
 summary(fit3sls)
